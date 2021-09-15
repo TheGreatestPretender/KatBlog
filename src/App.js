e721bfe7d9072bce31ce99cb4713d1629fb02b34
TheGreatestPretender <katschmidt201@gmail.com>
2021-09-15 15:12:43 -0400

    Fixed bug and moved cardlist component to App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 //personal components
 import Header from './components/Header';
 import About from './components/About';
-import CardList from './components/Card/CardList';
 
 import axios from 'axios';
+import {
+  Card,
+  CardContent,
+} from '@material-ui/core';
+
 
 //styling
 import './App.css';
 
 function App() {
 
-  let repoArr = [];
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const handleCardClick = () => {
+    console.log('clicked')
+  }
+
   useEffect(() => {
-
-
-    axios.get(`https://api.github.com/users/TheGreatestPretender/repos`)
-    .then(data => {
-      data = data.data
-      data.forEach(item => repoArr.push(item.html_url))
-    })
-
-    console.log(repoArr)
-  });
+    setTimeout(async () => {
+      let urls = [];
+      await axios.get(`https://api.github.com/users/TheGreatestPretender/repos`)
+      .then(data => {
+        data = data.data;
+        data.forEach(item => urls.push(item.html_url))
+        setRepos(urls)
+      })
+      setLoading(false)
+    }, 5000)
+  }, []);
 
   return (
     <div className="App">
@@ -31,11 +42,23 @@ function App() {
       <h2>I'm a full-stack engineer who identifies as&nbsp; <a href="https://transequality.org/issues/resources/understanding-non-binary-people-how-to-be-respectful-and-supportive">non-binary</a></h2>
       <About />
       <br/>
-      <CardList list={repoArr}/>
 
-      <p>
-        Links to projects
-      </p>
+      {
+        loading ? 
+          <i class="fas fa-spinner"/>
+          :
+          repos.map((entry, idx) => {
+            return (
+            <Card onClick={handleCardClick} className="card container">
+              <CardContent>
+                  <a href={entry}>
+                      {entry}
+                  </a>
+              </CardContent>
+            </Card>)
+          })
+      }
+
     </div>
   );
 }
